a2a0720e370d86f6c38ada52a0c491ba54d813d7
Malik <Mateusz.Malik@hidglobal.com>
2019-11-24 16:16:36 +0100

    update

--- vars/windows.groovy
+++ vars/windows.groovy
@@ -1,6 +1,14 @@
-def call(Closure body) {
-    body()
-    // node('windows') {
-    //     body()
-    // }
+@Grab('org.apache.commons:commons-math3:3.4.1')
+import org.apache.commons.math3.primes.Primes
+
+def call(count) {
+    parallelize(count)
+}
+
+
+void parallelize(int count) {
+  if (!Primes.isPrime(count)) {
+    error "${count} was not prime"
+  }
+  // …
 }
