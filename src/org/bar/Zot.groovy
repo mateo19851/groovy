9a64bc2095b386a42beee772209e8fa599b94e85
Malik <Mateusz.Malik@hidglobal.com>
2019-11-24 15:06:52 +0100

    update

--- src/org/bar/Zot.groovy
+++ src/org/bar/Zot.groovy
@@ -2,7 +2,7 @@ package org.bar
 
 class Zot implements Serializable {
   def steps
-  Utilities(steps) {this.steps = steps}
+  Zot(steps) {this.steps = steps}
   def mvn(args) {
     steps.sh "${steps.tool 'Maven'}/bin/mvn -o ${args}"
   }
