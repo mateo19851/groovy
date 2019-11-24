5530bfaa28090ce8d3a2d4026d21e73135fdc698
Malik <Mateusz.Malik@hidglobal.com>
2019-11-24 15:14:54 +0100

    update

--- src/org/bar/Utilities.groovy
+++ src/org/bar/Utilities.groovy
@@ -2,7 +2,11 @@ package org.bar
 
 class Utilities implements Serializable {
   def steps
-  Utilities(steps) {this.steps = steps}
+  Utilities(steps) {
+    this.steps = steps
+    println(steps)
+  }
+
   def mvn(args) {
     steps.sh "${steps.tool 'Maven'}/bin/mvn -o ${args}"
   }
