1e92edebdf3ace03d0fc95d9f44626c9a4ec5a17
Malik <Mateusz.Malik@hidglobal.com>
2019-11-24 15:21:40 +0100

    update

--- src/org/bar/Utilities.groovy
+++ src/org/bar/Utilities.groovy
@@ -8,7 +8,7 @@ class Utilities implements Serializable {
   }
 
   def mvn(args) {
-    println('test 12312312312312312312===========')
-    println(steps)
+    println 'test 12312312312312312312==========='
+    println steps
   }
 }
