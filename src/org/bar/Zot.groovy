09cc05817a7b34d097caa1033c9a62f390eb01f1
Malik <Mateusz.Malik@hidglobal.com>
2019-11-24 15:02:20 +0100

    update

--- src/org/bar/Zot.groovy
+++ src/org/bar/Zot.groovy
@@ -1,7 +1,9 @@
 package org.foo
 
-def checkOutFrom(repo) {
-  git url: "git@github.com:jenkinsci/${repo}"
+class Utilities implements Serializable {
+  def steps
+  Utilities(steps) {this.steps = steps}
+  def mvn(args) {
+    steps.sh "${steps.tool 'Maven'}/bin/mvn -o ${args}"
+  }
 }
-
-return this
